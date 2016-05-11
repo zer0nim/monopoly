769ae327b2a02f27cbae99d453832d4e297c84a1
marinern <marinern@pc-dg-037-02>
2016-05-11 14:18:04 +0200

    update

---
+++ src/Jeu/Controleur.java
@@ -0,0 +1,13 @@
+package Jeu;
+import Data.Ihm;
+
+public class Controleur {
+	public Ihm ihm;
+	public Monopoly monopoly;
+
+	public void jouerUnCoup(Joueur J) {
+	}
+
+	private Carreau lancerDésAvancer(Joueur J) {
+	}
+}
