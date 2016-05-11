0142fa9aebd19a2039a264ae6d36cce0aba8133a
marinern <marinern@pc-dg-037-02>
2016-05-11 13:53:28 +0200

    test

--- src/monopoly/Data/Carreau.java
+++ src/monopoly/Data/Carreau.java
@@ -1,7 +1,7 @@
 package monopoly.Data;
 
 public abstract class Carreau {
-	private int _numero;
+	private int numero;
 	private String _nomCarreau;
 	public Groupe _groupe;
 	public AutreCarreau _unnamed_AutreCarreau_;
