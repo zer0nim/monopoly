ab5b7067d8b5069d1d0bd36b66401bdcf0671613
marinern <marinern@pc-dg-037-02>
2016-05-11 14:52:31 +0200

    update

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import Data.Carreau;
 
 public class Monopoly {
         private Arraylist<Carreau> carreaux;
