d1c9154747032e7b67dccf83f5b482cb2cb294a1
marinern <marinern@pc-dg-025-15>
2016-05-25 15:02:19 +0200

    update

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -11,7 +11,7 @@ import Data.*;
 public class Monopoly {
     private ArrayList<Carreau> carreaux;
     private ArrayList<Joueur> joueurs;
-    
+    private ArrayList<Groupe> groupes;
     
     
     public Monopoly() {
