48df59baa7cc60fdb1fa2f11dba325e1c03386bb
marinern <marinern@pc-dg-025-15>
2016-05-31 08:12:06 +0200

    update

---
+++ src/Data/EnumerationPion.java
@@ -0,0 +1,8 @@
+package Data;
+
+
+public class EnumerationPion {
+    public static final String [] PIONS = { "Chapeau", "Voiture", "Bateau",
+	"Dé à coudre",	"Chaussure", "Brouette", "Chien", "Fer à repasser",
+	"Cavalier", "Canon", "Sac de Billets"};
+}
