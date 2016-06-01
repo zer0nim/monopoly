d20e4ed5c9c3deae621a19861e27eb95fb7a7c46
marinern <marinern@pc-dg-025-15>
2016-06-01 10:14:13 +0200

    update

--- src/Data/AutreCarreau.java
+++ src/Data/AutreCarreau.java
@@ -13,28 +13,28 @@ public class AutreCarreau extends Carreau{
 	switch (getNomCarreau()) {
 	    case "Chance":
 		
-		System.out.print("A Faire ! Action Chance");
+		System.out.println("A Faire ! Action Chance");
 		break;
 	    case "Caisse de Communauté":
-		System.out.print("A Faire ! Action Caisse de Communauté");
+		System.out.println("A Faire ! Action Caisse de Communauté");
 		break;
 	    case "Départ":
-		System.out.print("A Faire ! Action Départ");
+		System.out.println("A Faire ! Action Départ");
 		break;
 	    case "Impôt sur le revenu":
-		System.out.print("A Faire ! Action Impôt sur le revenu");
+		System.out.println("A Faire ! Action Impôt sur le revenu");
 		break;
 	    case "Simple Visite / En Prison":
-		System.out.print("A Faire ! Action Simple Visite / En Prison");
+		System.out.println("A Faire ! Action Simple Visite / En Prison");
 		break;
 	    case "Parc Gratuit":
-		System.out.print("A Faire ! Action Parc Gratuit");
+		System.out.println("A Faire ! Action Parc Gratuit");
 		break;
 	    case "Allez en prison":
-		System.out.print("A Faire ! Action Allez en prison");
+		System.out.println("A Faire ! Action Allez en prison");
 		break;
 	    case "Taxe de Luxe":
-		System.out.print("A Faire ! Action Taxe de Luxe");
+		System.out.println("A Faire ! Action Taxe de Luxe");
 		break;
 	    default:
 		break;
