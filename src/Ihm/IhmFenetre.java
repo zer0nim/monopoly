e5e552a272645a517287a4db8e361c41cd82b4d5
marinern <marinern@pc-dg-035-09>
2016-06-07 16:06:24 +0200

    Merge origin/master
    Conflicts:
    	src/Data/AutreCarreau.java

--- src/Ihm/IhmFenetre.java
+++ src/Ihm/IhmFenetre.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ihm;
 
-import Jeu.Monopoly;
-import main.Main;
-import java.util.*;
 import javax.swing.*;
-import java.awt.*;
-/**
- *
- * @author albertar
- */
+
 public class IhmFenetre extends JPanel {
     public IhmFenetre() {
 	JFrame frame = new JFrame();
